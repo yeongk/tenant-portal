3aaa69d6188b544854c953ecf0cc6a58210baba6
Yeong Kim <ykim4@homesite.com>
2026-04-17 15:57:10 -0400

    .env.<env> files added; vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
 export default defineConfig({
-  plugins: [react()],
-  server: { port: 5174 },
+  plugins: [
+    react(),
+    svgr(),
+  ],
 })
